cbe6f42ad2959d5fed1693a74ec65a9baf472fec
rajchandra3 <rajchandra963@gmail.com>
2020-03-22 13:51:29 +0530

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
 git add .
 git commit -m "$1"
 git push
+
+#push in the other repo as well
+cd ..
+rm -rf ./corona-go
+mkdir ./corona-go
+cp ./fight-covid19 ./corona-go
+cd ./corona-go
+git init
+git add .
+git commit -m "$1"
+git remote add origin git@github.com:rajchandra3/corona-go.git
+git push -u origin master
+
